2114fd5d46ce6605163653382e1003c018801f5f
unknown <zloymax31@gmail.com>
2023-08-06 18:11:08 +0300

    finished

---
+++ index.js
@@ -0,0 +1,62 @@
+let counter = 0;
+while (counter <= 10) {
+  console.log("counter: ", counter);
+  counter += 1;
+}
+
+// asd
+
+const number = 20;
+for (let i = 2; i < number; i += 1) {
+  if (i % 2 === 1) {
+    continue;
+  }
+  console.log("парне i: ", i);
+}
+
+// asd
+
+const numberTwo = 7;
+for (let ii = 1; ii <= 10; ii += 1) {
+  res = numberTwo * ii;
+  console.log("таблиця множення на 7: ", res);
+}
+
+// asd
+
+const clients = [1, 2, 3, 4, 5];
+let iiiiii = 0;
+while (iiiiii < clients.length) {
+    console.log("масив: ", clients[iiiiii]);
+    iiiiii += 1
+}
+
+// asd
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+for (let iii = 0; numbers.length > iii; iii += 1) {
+  if (iii === 7) {
+    break;
+}
+    console.log("iii: ",numbers[iii]);
+}
+
+// asd
+
+let n = 10;
+for (let iiii = 0; iiii < n; iiii += 1) {
+    console.log(iiii);
+    if (iiii >= n) {
+        break
+    }
+}
+
+// asd
+
+const numberThree = 20;
+for (let iiiii = 0; iiiii <= numberThree; iiiii += 1) {
+  if (iiiii % 3 === 0) {
+    continue;
+  }
+  console.log("iiiii: ", iiiii);
+}
